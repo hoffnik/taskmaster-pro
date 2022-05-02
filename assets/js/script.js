c045e78a78a02864d9f42d9ed82e738229e3bc64
hoffnik <hoffnik.dev@gmail.com>
2022-05-02 16:28:07 -0700

    drap and drop/ delete zone added, responsive design

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,6 +197,86 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// make ul elements sortable
+$('.card .list-group').sortable({
+  connectWith: $('.card .list-group'),
+  scroll: false,
+  tolerance: 'pointer',
+  /*  tells jQuery to create a copy of the dragged element and move the copy instead of the original. This is necessary to prevent click events from accidentally triggering on the original element */
+  helper: 'clone',
+  activate: function(event){
+    console.log('activate', this);
+  },
+  deactivate: function(event){
+    console.log('deactivate, this');
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+var tempArr = [];
+    
+
+
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+  // .find()  Get the descendants of each element in the current set of matched elements
+    .find("p")
+    // .text()  Get the combined text contents of each element in the set of matched elements
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+});
+console.log(tempArr);
+
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+
+  }
+});
+
+$('#trash').droppable({
+  // what items are accepted to be dropped
+  accept: '.card .list-group-item',
+  // when is an item hovering and when droppable
+  tolerance: 'touch',
+  // Triggered when an accepted draggable is dropped on the droppable
+  // ui is a function parameter that is an obkect, containing a property called draggable. Draggable represents the draggable element
+  drop: function(event, ui) {
+    console.log('drop');
+    ui.draggable.remove();
+  },
+  // Triggered when an accepted draggable is dragged over the droppable
+  over: function(event, ui) {
+    console.log("over");
+  },
+  // Triggered when an accepted draggable is dragged out of the droppable
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 // load tasks for the first time
 loadTasks();
 
